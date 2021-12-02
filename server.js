cbc7cc7030c7542d93ca10b4700b984b90635628
kamia700 <kaana757@gmail.com>
2021-12-02 18:00:40 +0100

    changing response

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => { res.send(database.users) });
+app.get('/', (req, res) => { res.send('it is working!') });
 // app.post('/signin', (req, res) => { signin.handleSignIn(req, res, db, bcrypt) }); or
 app.post('/signin', signin.handleSignIn(db, bcrypt));
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
